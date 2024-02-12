ddca1c7b41a75404b676a25d4e952e4ffb43ba78
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2024-02-13 01:38:46 +0530

    Add Book Store Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import Projects from './pages/Projects'
-import SignUp from './pages/SignUp'
-import WelcomePage from './pages/Welcome.jsx';
-import ChallengesPage from './pages/Challenges.jsx';
-import QuizPage from './pages/QuizPage.jsx';
-import Header from './components/Header/Header.jsx'
-import Notes from './pages/Notes.jsx'
-import Contact from './components/Contact/Contact.jsx'
-import Footer from './components/Footer/Footer.jsx'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
+import WelcomePage from "./pages/Welcome.jsx";
+import ChallengesPage from "./pages/Challenges.jsx";
+import QuizPage from "./pages/QuizPage.jsx";
+import Header from "./components/Header/Header.jsx";
+import Notes from "./pages/Notes.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="" element={<WelcomePage />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/projects" element={<Projects />} />
@@ -29,7 +26,7 @@ export default function App() {
         <Route path="notes" element={<Notes />} />
         <Route path="quiz" element={<QuizPage />} />
       </Routes>
-        <Footer />
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
