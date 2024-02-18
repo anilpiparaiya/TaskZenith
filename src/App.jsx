404f5a26023b8fac8b8840068b505a25a7a71451
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2024-02-18 12:02:52 +0530

    make the dashboard private

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
+import Dashboard from './pages/Dashboard';
 import Projects from "./pages/Projects";
 import SignUp from "./pages/SignUp";
 import WelcomePage from "./pages/Welcome.jsx";
@@ -11,7 +12,7 @@ import Header from "./components/Header/Header.jsx";
 import Notes from "./pages/Notes.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Codeforces from "./pages/Codeforces.jsx";
-
+import PrivateRoute from './components/Header/PrivateRoute.jsx';
 export default function App() {
   return (
     <BrowserRouter>
@@ -22,6 +23,9 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
         <Route path="/challenges" element={<ChallengesPage />} />
         <Route path="notes" element={<Notes />} />
