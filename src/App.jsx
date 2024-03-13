555616c7b5d8748e358b275c70661769c46d3f1d
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2024-03-13 15:49:01 +0530

    Add favorite places functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
-import Dashboard from './pages/Dashboard';
+import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import SignUp from "./pages/SignUp";
 import WelcomePage from "./pages/Welcome.jsx";
@@ -9,13 +9,14 @@ import ChallengesPage from "./pages/Challenges.jsx";
 import Header from "./components/Header/Header.jsx";
 import Notes from "./pages/Notes.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import PrivateRoute from './components/Header/PrivateRoute.jsx';
+import PrivateRoute from "./components/Header/PrivateRoute.jsx";
 import BookNotes from "./components/BookNotes/BookNotes.jsx";
-import BookHome from './pages/books/Home';
-import CreateBook from './pages/books/CreateBooks';
-import ShowBook from './pages/books/ShowBook';
-import EditBook from './pages/books/EditBook';
-import DeleteBook from './pages/books/DeleteBook';
+import BookHome from "./pages/books/Home";
+import CreateBook from "./pages/books/CreateBooks";
+import ShowBook from "./pages/books/ShowBook";
+import EditBook from "./pages/books/EditBook";
+import DeleteBook from "./pages/books/DeleteBook";
+import PlacePicker from "./pages/PlacePicker.jsx";
 export default function App() {
   return (
     <BrowserRouter>
@@ -26,19 +27,19 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route element={<PrivateRoute />}>
-          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Route>
         <Route path="/projects" element={<Projects />} />
         <Route path="/challenges" element={<ChallengesPage />} />
         <Route path="notes" element={<Notes />} />
         <Route path="booknotes" element={<BookNotes />} />
 
-      <Route path='/books' element={<BookHome />} />
-      <Route path='/books/create' element={<CreateBook />} />
-      <Route path='/books/details/:id' element={<ShowBook />} />
-      <Route path='/books/edit/:id' element={<EditBook />} />
-      <Route path='/books/delete/:id' element={<DeleteBook />} />
-
+        <Route path="/books" element={<BookHome />} />
+        <Route path="/books/create" element={<CreateBook />} />
+        <Route path="/books/details/:id" element={<ShowBook />} />
+        <Route path="/books/edit/:id" element={<EditBook />} />
+        <Route path="/books/delete/:id" element={<DeleteBook />} />
+        <Route path="/places" element={<PlacePicker />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon, FaSun } from 'react-icons/fa';
-import { useSelector, useDispatch } from 'react-redux';
-import { toggleTheme } from '../../redux/theme/themeSlice';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../../redux/theme/themeSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
@@ -22,47 +22,47 @@ export default function Header() {
       </Link>
       <form>
         <TextInput
-          type='text'
-          placeholder='Search...'
+          type="text"
+          placeholder="Search..."
           rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'
+          className="hidden lg:inline"
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
-      <div className='flex gap-2 md:order-2'>
+      <div className="flex gap-2 md:order-2">
         <Button
-          className='w-12 h-10 hidden sm:inline'
-          color='gray'
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
           pill
           onClick={() => dispatch(toggleTheme())}
         >
-          {theme === 'light' ? <FaSun /> : <FaMoon />}
+          {theme === "light" ? <FaSun /> : <FaMoon />}
         </Button>
         {currentUser ? (
           <Dropdown
             arrowIcon={false}
             inline
             label={
-              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
             }
           >
             <Dropdown.Header>
-              <span className='block text-sm'>@{currentUser.username}</span>
-              <span className='block text-sm font-medium truncate'>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
                 {currentUser.email}
               </span>
             </Dropdown.Header>
-            <Link to={'/dashboard?tab=profile'}>
+            <Link to={"/dashboard?tab=profile"}>
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
             <Dropdown.Item>Sign out</Dropdown.Item>
           </Dropdown>
         ) : (
-          <Link to='/sign-in'>
-            <Button gradientDuoTone='purpleToBlue' outline>
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
               Sign In
             </Button>
           </Link>
@@ -93,8 +93,9 @@ export default function Header() {
           <Link to="/notes">Notes</Link>
         </Navbar.Link>
 
-        
-
+        <Navbar.Link active={path === "/places"} as={"div"}>
+          <Link to="/places">Places</Link>
+        </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
