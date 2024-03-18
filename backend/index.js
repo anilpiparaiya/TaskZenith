55947e040453b9440bd2808c5c814c1abe01867a
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2024-03-18 17:47:25 +0530

    add update user API route

--- backend/index.js
+++ backend/index.js
@@ -4,23 +4,24 @@ import { PORT } from "./config.js";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-import booksRoute from './routes/booksRoute.js';
-import cors from 'cors';
+import booksRoute from "./routes/booksRoute.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+app.use(cookieParser());
 
 // Middleware for handling CORS POLICY
- app.use(cors());
+app.use(cors());
 
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
   console.log(request);
-  return response.status(234).send('Welcome To TaskZenith');
+  return response.status(234).send("Welcome To TaskZenith");
 });
-
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -34,12 +35,9 @@ mongoose
     console.log(err);
   });
 
-
-
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-app.use('/api/books', booksRoute);
-
+app.use("/api/books", booksRoute);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
