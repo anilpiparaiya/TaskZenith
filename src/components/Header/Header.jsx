fbf4255b7dd9e3ee6388ad781b9ba88b5f3d9967
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2024-03-27 16:31:09 +0530

    remove projects

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -81,9 +81,6 @@ export default function Header() {
         <Navbar.Link active={path === "/search-book"} as={"div"}>
           <Link to="/search-book">Books</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/projects"} as={"div"}>
-          <Link to="/projects">Projects</Link>
-        </Navbar.Link>
 
         <Navbar.Link active={path === "/challenges"} as={"div"}>
           <Link to="/challenges">Challenge</Link>
