1f6e3dc0eb974d0a3b3a6e3f94e494a87c27f25d
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2024-04-29 12:43:24 +0530

    add Meme Generator

---
+++ src/components/Memes/MemeSearch.jsx
@@ -0,0 +1,25 @@
+// MemeSearch.js
+import React from "react";
+import "../../pages/memes/style.css";
+import "../../index.css";
+
+const MemeSearch = ({ searchQuery, setSearchQuery }) => {
+  const handleSearchChange = (event) => {
+    // Update the search query state in the parent component
+    setSearchQuery(event.target.value);
+  };
+
+  return (
+    <div className="meme-search text-center mt-20">
+      <input
+        className="mem w-full max-w-300 p-2 border-2 border-gray-300 rounded-20 text-2xl outline-none"
+        type="text"
+        placeholder="Search Memes"
+        value={searchQuery}
+        onChange={handleSearchChange}
+      />
+    </div>
+  );
+};
+
+export default MemeSearch;
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import NoteContainer from "../components/NoteContainer/NoteContainer";
 import Sidebar from "../components/Sidebar/Sidebar";
+import { Link } from "react-router-dom";
 
 function App() {
   const [notes, setNotes] = useState(
@@ -44,6 +45,31 @@ function App() {
   }, [notes]);
   return (
     <div className="App">
+      <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
+        <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
+          <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
+            <h2 className="text-4xl font-bold sm:text-5xl">
+              Welcome Back
+              <span className="hidden sm:block text-4xl">Go to Notes Tab</span>
+            </h2>
+
+            <Link
+              className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
+              to="/notes"
+            >
+              &nbsp; Make Notes Now
+            </Link>
+          </div>
+        </div>
+
+        <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
+          <img
+            className="w-96"
+            src="https://i.ibb.co/5BCcDYB/Remote2.png"
+            alt="image1"
+          />
+        </div>
+      </aside>
       <Sidebar addNote={addNote} />
       <NoteContainer
         notes={notes}
